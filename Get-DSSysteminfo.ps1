6c420f27fa1c7c5be53248f7e94ab3a2acd2399d
Daniel Schwensen <daniel.schwensen@outlook.com>
2018-01-27 15:28:50 +0100

    x

--- Get-DSSysteminfo.ps1
+++ Get-DSSysteminfo.ps1
@@ -1,27 +1,5 @@
 function Get-DSSysteminfo() {
-    <#
-.SYNOPSIS
-Retrieves key system version and model information
-from one to ten computers.
-.DESCRIPTION
-Get-SystemInfo uses Windows Management Instrumentation
-(WMI) to retrieve information from one or more computers.
-Specify computers by name or by IP address.
-.PARAMETER ComputerName
-One or more computer names or IP addresses, up to a maximum
-of 10.
-.PARAMETER LogErrors
-Specify this switch to create a text log file of computers
-that could not be queried.
-.PARAMETER ErrorLog
-When used with -LogErrors, specifies the file path and name
-to which failed computer names will be written. Defaults to
-C:\Retry.txt.
-.EXAMPLE
- Get-Content names.txt | Get-DSSystemInfo
-.EXAMPLE
- Get-DSSystemInfo -ComputerName SERVER1,SERVER2
-#>
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
@@ -33,12 +11,6 @@ C:\Retry.txt.
 
     BEGIN { Write-Output "Log name is $errorLog" -Verbose
     }
-
-    # If the function is called with nothing more than its parameters, meaning no information is piped into it from the
-    # pipeline, then the PROCESSS block runs one time for each object that was piped in from the pipeline.
-    #
-    #If the function is called with pipeline input, then the PROCESS block runs one time for each object
-    # that was piped in from the pipeline.
 
     PROCESS {
         foreach ($computer in $computerName) {
